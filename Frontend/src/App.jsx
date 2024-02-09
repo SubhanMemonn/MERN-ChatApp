92fa9ab3e4880f95e08e5798caa13e66d7de6058
subhan-saleem7 <subhanmemon131@gmail.com>
2024-02-10 01:34:02 +0500

    Resolve some bugs in listenMessage hook and app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,10 +8,9 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import { useAuthContext } from "./context/AuthContext";
 function App() {
-  const { authUser } = useContext(AuthContext);
+  const { authUser } = useAuthContext();
   return (
     <div className="sm:p-4 h-screen flex items-center justify-center">
       <Router>
--- Frontend/src/hooks/useListenMessages.js
+++ Frontend/src/hooks/useListenMessages.js
@@ -2,22 +2,28 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation"
 import notificationSound from "../assets/sounds/notification.mp3";
+import { useAuthContext } from "../context/AuthContext";
 
 const useListenMessages = () => {
     const { socket } = useSocketContext();
     const { messages, setMessages, selectedConversation } = useConversation();
-    console.log(selectedConversation._id);
+    const { authUser } = useAuthContext();
+    console.log("selected id", selectedConversation._id);
+    console.log("Auth user", authUser._id);
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
+            console.log(newMessage);
             const sound = new Audio(notificationSound);
             sound.play();
-            if (selectedConversation?._id == newMessage.receiverId ||
-                newMessage.senderId) {
+            if (selectedConversation?._id === newMessage.senderId) {
                 setMessages([...messages, newMessage])
             }
         })
 
-        return () => socket?.off("newMessage")
+        return () => {
+            socket?.off("newMessage");
+            // setMessages([])
+        }
     }, [messages, socket, setMessages, selectedConversation])
 }
 export default useListenMessages
